b670cc82d2ac30cc2a7192b3248d99ca8aa108a8
OwerHub <owerwild@gmail.com>
2021-12-18 17:07:17 +0100

    add php backend

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1>Hello Vue</h1>
+    </div>
+    <div>
+      <p>{{fixedDataMessage}}</p>
+    </div>
+    <div class="bucketlist">
+      <h2>BucketList</h2>
+      <ul>
+        <li v-for="task in tasks" v-bind:key="task.id" :class="{completedClass: task.completed}">
+           <div> <span>Task: </span>
+           <span>{{task.name}}</span>
+           </div>
+           <div>
+             <span>Until: </span>
+             <span>{{task.due}}</span>
+           </div>
+           <div>
+             <input type="checkbox" :checked=task.completed @click="changeCompleted(task.id)">
+             <span> Done</span>
+           </div>
+        </li>
+      </ul>
+    </div>
+<!--     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
   name: 'App',
+  
+  // used components
   components: {
-    HelloWorld
+  },
+
+// fixed datas, loaded before the component render. 
+// or not. I can change the task.id and it change the class... ??????
+data(){
+  return{
+    fixedDataMessage: "fix data, what U use in the components. This loaded, when the componens loaded",
+    tasks:   [
+    {
+      id:1,
+      name:"Listen every music",
+      completed: false,
+      due: "2025-12-12"
+    },    
+    {
+      id:2,
+      name:"Join every festival",
+      completed: false,
+      due: "2025-12-22"
+    },
+        {
+      id:3,
+      name:"Drink every good shit",
+      completed: true,
+      due: "2028-12-22"
+    },
+        {
+      id:4,
+      name:"Fuck every hot girl",
+      completed: false,
+      due: "2027-12-30"
+    }
+    ]
+
+  };
+
+}, 
+
+// dynamic datas
+computed:{
+  }, 
+
+  //a lifcycled hook, 
+  created(){
+  },
+
+
+// define functions here
+methods:{
+  changeCompleted(id){
+     let task = this.tasks.find(task => task.id === id)
+    task.completed = !task.completed 
   }
 }
+
+
+}
+
+
 </script>
 
 <style>
@@ -24,5 +105,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  ;
+}
+
+.bucketlist{
+  margin: 0px 5rem;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.bucketlist li{
+  text-align: start;
+  margin: 1rem 0 0 0;
+  
+}
+
+.bucketlist li div span:first-child{
+  font-weight: bold;
+}
+
+.completedClass{
+  background-color: rgba(255, 0, 0, .3);
 }
 </style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<div class="udvozletDiv">I am {{propsName}}</div>
+	</div>
+</template>
+
+
+<script>
+export default{
+	name: "FirstComponent",
+	props:{
+		propsName: String
+	}
+}
+</script>
